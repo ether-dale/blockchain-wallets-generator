0831fc48607d8d40f7e6ae75ba51cf8617cbc2fa
andrei <andrei@etherdale.com>
2019-01-31 10:49:05 +0200

    install 3-rd party tools script

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 git clone https://github.com/maandree/libkeccak
 cd libkeccak
 make
-sudo make install PREFIX=/usr
+make install PREFIX=/usr
 cd
-sudo ln -s /usr/local/lib/libkeccak.so.1 /lib64/
+ln -s /usr/local/lib/libkeccak.so.1 /lib64/
 
 git clone https://github.com/maandree/sha3sum
 cd sha3sum
 make
-sudo make install
+make install
